10e97dc81dd9f57293fb3759a8e157912d121bf6
unknown <epam@b1a24t29.local.rsu.edu.ru>
2018-10-23 20:29:55 +0400

    update task5

--- src/ex5_js-functions/js_task5_01.js
+++ src/ex5_js-functions/js_task5_01.js
@@ -5,6 +5,9 @@ function Calculator(){
     function getResult(){
         return value;
     }
+    function reset(){
+        return value=0;
+    }
     function add(num=0){
         value+=num;
         return add;
@@ -21,26 +24,27 @@ function Calculator(){
         value*=num;
         return multiply;
     }
-    return {getResult, add, subtract, divide, multiply};
+    return {getResult, reset, add, subtract, divide, multiply};
 }
 
-var calclator = new Calculator();
+var calculator = new Calculator();
 
-console.log(calclator.getResult()); // 0
-calclator.add(4);
-calclator.subtract(1);
-console.log(calclator.getResult()); // 3
-calclator.subtract(3);
-console.log(calclator.getResult()); // 0
-calclator.add(4)(1);
-console.log(calclator.getResult()); // 5
-calclator.subtract(1)(1)(1)(1)(1);
-console.log(calclator.getResult()); // 0
-calclator.multiply(5);
-console.log(calclator.getResult()); // 0
-calclator.add(1)(1)(1)(2); //5
-console.log(calclator.getResult()); // 5
-calclator.multiply(2)(5);
-console.log(calclator.getResult()); // 50
-calclator.divide(2);
-console.log(calclator.getResult()); // 25
+console.log(calculator.getResult()); // 0
+calculator.add(4);
+calculator.subtract(1);
+console.log(calculator.getResult()); // 3
+calculator.subtract(3);
+console.log(calculator.getResult()); // 0
+calculator.add(4)(1);
+console.log(calculator.getResult()); // 5
+calculator.subtract(1)(1)(1)(1)(1);
+console.log(calculator.getResult()); // 0
+calculator.multiply(5);
+console.log(calculator.getResult()); // 0
+calculator.add(1)(1)(1)(2); //5
+console.log(calculator.getResult()); // 5
+calculator.multiply(2)(5);
+console.log(calculator.getResult()); // 50
+calculator.divide(2);
+console.log(calculator.getResult()); // 25
+console.log(calculator.reset());
