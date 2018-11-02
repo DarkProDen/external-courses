a42716539a6963c5639a5fdeeea2b823e932b69c
DarkProDen <denisvilkov666@gmail.com>
2018-11-02 09:38:35 +0300

    update task2
    for (var i=0;...)

--- src/ex2_js-basics-part2/js_task2_05.js
+++ src/ex2_js-basics-part2/js_task2_05.js
@@ -1,7 +1,6 @@
 var getMax=function(arr){
-	var i;
 	var max=arr[0]
-	for (i=1; i<arr.length; i++)
+	for (var i=1; i<arr.length; i++)
 		if (arr[i-1]<arr[i])
 			max=arr[i];
 	console.log("Максимальное значение: "+max);
