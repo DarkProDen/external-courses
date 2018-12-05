4a86535db68d0c6a6f3af805e7a4ff182692e3ef
DarkProDen <Denisvilkov666@gmail.com>
2018-12-05 19:39:15 +0300

    Update Classwork.js
    Constructors saved.

--- src/ex15_js-oop/Classwork.js
+++ src/ex15_js-oop/Classwork.js
@@ -19,14 +19,10 @@ var Triangle = function (name, a, b, c) {
     this.c = c;
 }
 Triangle.prototype = shape;
+Triangle.prototype.constructor = Triangle;
 Triangle.prototype.getPerimeter = function () {
     return this.a + this.b + this.c;
 }
-
-var tr = new Triangle('tr1', 1, 2, 3);
-tr.draw();
-console.log('Type: ' + tr.getType());
-console.log('Perimeter: ' + tr.getPerimeter());
 
 var Square = function (name, a) {
     this.type = 'Square';
@@ -34,6 +30,7 @@ var Square = function (name, a) {
     this.a = a;
 }
 Square.prototype = shape;
+Square.prototype.constructor = Square;
 Square.prototype.getPerimeter = function () {
     return this.a * 4;
 }
@@ -44,10 +41,21 @@ var Rectangle = function (name, a, b) {
     this.a = a;
     this.b = b;
 }
-Rectangle.prototype = new Square();
+Rectangle.prototype = Object.create(Square.prototype);
+Rectangle.prototype.constructor = Rectangle;
 Rectangle.prototype.getPerimeter = function () {
     return this.a * 2 + this.b * 2;
 }
+
+var tr = new Triangle('tr1', 1, 2, 3);
+tr.draw();
+console.log('Type: ' + tr.getType());
+console.log('Perimeter: ' + tr.getPerimeter());
+
+var sq = new Square('sq1', 2);
+sq.draw();
+console.log('Type: ' + sq.getType());
+console.log('Perimeter: ' + sq.getPerimeter());
 
 var rec = new Rectangle('rec1', 1, 2);
 rec.draw();
