b8aa735058fe8b344292dffeb8e78572b1cab971
DarkProDen <Denisvilkov666@gmail.com>
2018-12-05 20:53:31 +0300

    Update Task15.js
    Changes:
    Device.prototype.constructor = Device;
    Device.apply(this, arguments);
    Refrigerator.prototype = Object.create(Device.prototype);

--- src/ex15_js-oop/Task15.js
+++ src/ex15_js-oop/Task15.js
@@ -4,21 +4,22 @@ var Device = function (name, power) {
     this.power = power;
     this.enabled = false;
 }
+Device.prototype.constructor = Device;
 Device.prototype.switchOnOff = function () {
     if (this.enabled) {
         this.enabled = false;
-        console.log(this.name+' disabled');
+        console.log(this.name + ' disabled');
     }
     else {
         this.enabled = true;
-        console.log(this.name+' enabled');
+        console.log(this.name + ' enabled');
     }
 }
 Device.prototype.print = function () {
     for (var propertyName in this) {
-        if (typeof this[propertyName]!=='function') {
+        if (typeof this[propertyName] !== 'function') {
             console.log(propertyName + ': ' + this[propertyName]);
-        }  
+        }
     }
 }
 Device.prototype.getPower = function () {
@@ -31,55 +32,54 @@ Device.prototype.getPower = function () {
 }
 
 var Lamp = function (name, power) {
-    this.name = name;
+    Device.apply(this, arguments);
     this.type = 'Lamp';
-    this.power = power;
-    this.enabled = false;
     this.luminousFlux = 800;
 }
-Lamp.prototype = new Device();
+Lamp.prototype = Object.create(Device.prototype);
+Lamp.prototype.constructor = Lamp;
 
 var Chandelier = function (name, power) {
-    this.name = name;
+    Device.apply(this, arguments);
     this.type = 'Chandelier';
-    this.power = power;
-    this.enabled = false;
     this.luminousFlux = 2000;
-    this.lamps = [new Lamp(name+'__lamp1', power / 3), new Lamp(name+'__lamp2', power / 3), new Lamp(name+'__lamp3', power / 3)];
+    this.lamps = [new Lamp(name + '__lamp1', power / 3), 
+        new Lamp(name + '__lamp2', power / 3), 
+        new Lamp(name + '__lamp3', power / 3)];
 }
-Chandelier.prototype = new Device();
+Chandelier.prototype = Object.create(Device.prototype);
+Chandelier.prototype.constructor = Chandelier;
 Chandelier.prototype.switchOnOff = function () {
-    for (let i = 0; i < Object(this.lamps).length; i++) {
-        const lamp = Object(this.lamps)[i];
+    for (let i = 0; i < this.lamps.length; i++) {
+        const lamp = this.lamps[i];
         lamp.switchOnOff();
     }
     if (this.enabled) {
         this.enabled = false;
-        console.log(this.name+' disabled');
+        console.log(this.name + ' disabled');
     }
     else {
         this.enabled = true;
-        console.log(this.name+' enabled');
+        console.log(this.name + ' enabled');
     }
 }
 
 var Refrigerator = function (name, power) {
-    this.name = name;
+    Device.apply(this, arguments);
     this.type = 'Refrigerator';
-    this.power = power;
-    this.enabled = false;
-    this.lamp = new Lamp(name+'__lamp', 10);
+    this.lamp = new Lamp(name + '__lamp', 10);
     this.opened = false;
 }
-Refrigerator.prototype = new Device();
+Refrigerator.prototype = Object.create(Device.prototype);
+Refrigerator.prototype.constructor = Refrigerator;
 Refrigerator.prototype.openClose = function () {
     if (this.opened) {
         this.opened = false;
-        console.log(this.name+' door closed');
+        console.log(this.name + ' door closed');
     }
     else {
         this.opened = true;
-        console.log(this.name+' door opened');
+        console.log(this.name + ' door opened');
     }
     this.lamp.switchOnOff();
 }
@@ -100,28 +100,28 @@ var Screen = function (name, power) {
     this.resolutionX = 1920;
     this.resolutionY = 1080;
 }
-Screen.prototype = new Device();
+Screen.prototype = Object.create(Device.prototype);
+Screen.prototype.constructor = Screen;
 
 var Room = function (name, devices) {
     this.name = name;
-    this.devices = devices;
-    /*
+    this.devices = [];
     devices.forEach(device => {
         this.devices.push(device);
     });
-    */
 }
+Room.prototype.constructor = Room;
 Room.prototype.getPower = function () {
     var result = 0;
-    for (let i = 0; i < Object(this.devices).length; i++) {
-        const device = Object(this.devices)[i];
+    for (let i = 0; i < this.devices.length; i++) {
+        const device = this.devices[i];
         result += device.getPower();
     }
     return result;
 }
 Room.prototype.getDevice = function (deviceName) {
-    for (let i = 0; i < Object(this.devices).length; i++) {
-        const device = Object(this.devices)[i];
+    for (let i = 0; i < this.devices.length; i++) {
+        const device = this.devices[i];
         if (device.name === deviceName) {
             return device;
         }
@@ -142,41 +142,29 @@ var room = new Room('Kitchen',
     new Refrigerator('HAIER C2F637CGG', 300),
     new Device('Microwave', 2000),
     new Screen('TV', 300)]);
-var i=0;
-room.devices.forEach(device => {
-    if (i%2===0) {
-        device.switchOnOff();
-    }
-    //device.print();
-    i++;
-});
-console.log('Room consuming '+room.getPower()+' W');
-room.devices.forEach(device => {
-    if (i%2===0) {
-        device.switchOnOff();
-    }
-    //device.print();
-    i++;
-});
-console.log('Room consuming '+room.getPower()+' W');
+
+for (let i = 0; i < room.devices.length; i+=2) {
+    const device = room.devices[i];
+    device.switchOnOff();
+}
+console.log('Room consuming ' + room.getPower() + ' W');
+for (let i = 1; i < room.devices.length; i+=2) {
+    const device = room.devices[i];
+    device.switchOnOff();
+}
+console.log('Room consuming ' + room.getPower() + ' W');
 room.getDevice('HAIER C2F637CGG').openClose();
-console.log('Room consuming '+room.getPower()+' W');
+console.log('Room consuming ' + room.getPower() + ' W');
 room.getDevice('HAIER C2F637CGG').openClose();
-console.log('Room consuming '+room.getPower()+' W');
-room.devices.forEach(device => {
-    if (i%2===0) {
-        device.switchOnOff();
-    }
-    //device.print();
-    i++;
-});
-console.log('Room consuming '+room.getPower()+' W');
-room.devices.forEach(device => {
-    if (i%2===0) {
-        device.switchOnOff();
-    }
-    //device.print();
-    i++;
-});
-console.log('Room consuming '+room.getPower()+' W');
+console.log('Room consuming ' + room.getPower() + ' W');
+for (let i = 0; i < room.devices.length; i+=2) {
+    const device = room.devices[i];
+    device.switchOnOff();
+}
+console.log('Room consuming ' + room.getPower() + ' W');
+for (let i = 1; i < room.devices.length; i+=2) {
+    const device = room.devices[i];
+    device.switchOnOff();
+}
+console.log('Room consuming ' + room.getPower() + ' W');
 
